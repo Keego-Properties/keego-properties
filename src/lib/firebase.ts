8acd8560542a0d7610ec6ef3f07b8ba25fc8f19d
Ramees Muhammed <rameesmuhammed957@gmail.com>
2026-04-08 12:13:00 +0530

    env added

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1RKR1SEfcf8SD80olwg9t2O9b_8XbZsw",
-  authDomain: "keego-properties.firebaseapp.com",
-  projectId: "keego-properties",
-  storageBucket: "keego-properties.firebasestorage.app",
-  messagingSenderId: "1005799627328",
-  appId: "1:1005799627328:web:ab6925cc3e33095c689da5",
-  measurementId: "G-58TFE124PH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
